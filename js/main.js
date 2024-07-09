2ae7d703b28432c652e87e69975b8208a80fb5c5
WilliamSepulveda <160752969+WilliamSepulveda@users.noreply.github.com>
2024-07-09 17:16:23 -0500

    feat :sparkles: se crea  una clase llamada Estudiante que se  hereda de la clase Persona
    se Creó  una clase llamada Estudiante que herede de la clase
    Persona. Agregando  una nueva propiedad llamada carrera y un
    método llamado estudiar que imprima en la consola un
    mensaje indicando la carreraque está estudiando

--- js/main.js
+++ js/main.js
@@ -7,9 +7,19 @@ class Personas {
 
 
   saludar() {
-    console.log(`Hola, mi nombre  es ${this.Nombre}.`)
+    console.log(`Hola, mi nombre  es ${this.Nombre}, tengo ${this.Edad} años, y mi genero es... ${this.Sexo},`)
   }
 }
+class Estudiante extends Personas{
+    constructor(Nombre, Edad, Sexo, Carrera){
+        super(Nombre, Edad, Sexo);
+        this.Carrera = Carrera;
+    }
+    estudiar(){
+        console.log(`actualmente estoy estudiando ${this.Carrera}.`)
+    }
+}
+
 document.addEventListener('DOMContentLoaded',()=>{
     const form = document.getElementById('registrationForm');
 
@@ -19,10 +29,17 @@ document.addEventListener('DOMContentLoaded',()=>{
         const nombre = document.getElementById('name').value;
         const edad = document.getElementById('age').value;
         const sexo = document.getElementById('gender').value;
+        const carrera = document.getElementById('carrera').value; 
 
-        const persona1 = new Personas(nombre, edad, sexo );
 
+        const persona1 = new Personas(nombre, edad, sexo, );
+        const Estudiante1 = new Estudiante(nombre, edad, sexo, carrera);
+
+        
         persona1.saludar();
+        Estudiante1.estudiar();
 
 });
 });
+
+
